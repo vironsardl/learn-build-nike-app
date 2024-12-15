4ec1bc415a1497eb8a39f53336b0025db7f2c74a
Vironsard Vang <vangvironsardw26@gmail.com>
2024-12-15 10:11:47 +0700

    imp color

--- App.js
+++ App.js
@@ -15,10 +15,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#000",
+    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
 });
 
-// Todo: 34:38 
+// Todo: 38:09 
