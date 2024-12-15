f59674ea19e24783da0f0a99b4d4ab939fcaec2f
Vironsard Vang <vangvironsardw26@gmail.com>
2024-12-15 11:53:08 +0700

    add - shopping cart

--- App.js
+++ App.js
@@ -2,14 +2,16 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, } from "react-native";
 import ProductsSreen from './src/screens/ProductsScreen';
 import ProductDetailScreen from "./src/screens/ProductDetailScreen";
+import ShoppingCart from "./src/screens/ShoppingCart";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
 
-     <ProductDetailScreen />
+     {/* <ProductDetailScreen /> */}
      {/* <ProductsSreen /> */}
+     <ShoppingCart />
     </View>
   );
 }
--- src/screens/ShoppingCart.js
+++ src/screens/ShoppingCart.js
@@ -1,87 +1,93 @@
-import React from 'react'
-import { FlatList, Pressable, StyleSheet, Text, View } from 'react-native';
-import CartListItem from '../components/CartListItem';
+import React from "react";
+import { FlatList, Pressable, StyleSheet, Text, View } from "react-native";
+import CartListItem from "../components/CartListItem";
+import cart from "../data/cart";
 
-export default function ShoppingCart() {
+
+const ShoppingCartTotals = () => (
+    <View style={styles.totalsContainer}>
+      <View style={styles.row}>
+        <Text style={styles.text}>Subtotal</Text>
+        <Text style={styles.text}>410,00 USD</Text>
+      </View>
+
+      <View style={styles.row}>
+        <Text style={styles.text}>Delivery</Text>
+        <Text style={styles.text}>10 USD</Text>
+      </View>
+
+      <View style={styles.row}>
+        <Text style={styles.textBold}>Total</Text>
+        <Text style={styles.textBold}>10 USD</Text>
+      </View>
+    </View>
+  )
+
+const ShoppingCart = () => {
+  const Checkout = () => {
+    console.log("Add checkout");
+  };
   return (
-    <FlatList 
+    <>
+      <FlatList
         data={cart}
-        renderItem={({item}) => <CartListItem cartItem={item}/>}  \
-        ListFooterComponents = {() => (
-            <View style={styles.totalsContainer}>
-                <View style={styles.row}>
-                    <Text style={styles.text}>SubTotal</Text>
-                    <Text style={styles.text}>410,00</Text>
-                </View>
-                <View style={styles.row}>
-                    <Text style={styles.text}>Delivery</Text>
-                    <Text style={styles.text}>16.50</Text>
-                </View>
-                <View style={styles.row}>
-                    <Text style={styles.textBold}>Total</Text>
-                    <Text style={styles.textBold}>426,50</Text>
-                </View>
-
-                <View style={styles.footer}>
-                    <Pressable style={styles.button}>
-                        <Text style={styles.buttonText}>Checkout</Text>
-                    </Pressable>
-                </View>
-
-            </View>
-            
-        )}
-        
-    />
-    
-    
-
+        renderItem={({ item }) => <CartListItem cartItem={item} />}
+        ListFooterComponent={ShoppingCartTotals()}
+      />
+      <Pressable onPress={Checkout} style={styles.button}>
+        <Text style={styles.buttonText}>Checkout</Text>
+      </Pressable>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
-    totalsContainer: {
-        margin: 20,
-        paddingTop: 10,
-        borderColor: "gainsboro",
-        borderTopWidth: 1,
-    },
-    row: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        marginVertical: 2,
-    },
-    text: {
-        fontSize: 16,
-        color: "gray",
+  totalsContainer: {
+    margin: 20,
+    paddingTop: 10,
+    borderColor: "gainsboro",
+    borderTopWidth: 1,
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginVertical: 2,
+  },
+  text: {
+    fontSize: 16,
+    color: "gray",
+  },
 
-    },
+  textBold: {
+    fontSize: 16,
+    fontWeight: "500",
+  },
+  // footer: {
+  //     position: "absolute",
+  //     bottom: 0,
+  //     width: "100%",
+  //     backgroundColor: "white",
+  //     borderColor: "gainsboro",
+  //     borderTopWidth: 1,
+  //     padding: 20,
+  // },
 
-    textBold: {
-        fontSize: 16,
-        fontWeight: "500",
-    },
-    footer: {
-        position: "absolute",
-        bottom: 0,
-        width: "100%",
-        backgroundColor: "white",
-        borderColor: "gainsboro",
-        borderTopWidth: 1,
-        padding: 20,
-    },
+  button: {
+    position: "absolute",
+    width: "90%",
+    backgroundColor: "black",
+    bottom: 30,
+    alignItems: 'center',
+    padding: 20,
+    borderRadius: 100,
+    alignSelf: 'center',
 
-    button: {
-        width: "100%",
-        backgroundColor: "black",
-        alignSelf: "center",
-        alignItem: "center",
-        padding: 20,
-        borderRadius: 200,
-    },
-    buttonText: {
-        color: 'white',
-        fontWeight: "500",
-        fontSize: 16,
-    }
-})
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "500",
+    fontSize: 16,
+  },
+});
+
+export default ShoppingCart;
